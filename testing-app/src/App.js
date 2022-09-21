ed57b6d53877d127f7ec4d75c785cd522b7ec2cb
prooheckcp <61127277+prooheckcp@users.noreply.github.com>
2022-09-21 22:55:21 +0100

    Testing attributes props

--- testing-app/src/App.js
+++ testing-app/src/App.js
@@ -1,9 +1,24 @@
 import './App.css';
 
+const Person = (props) => {
+  return (
+    <>
+      <h1>Name : {props.name}</h1>
+      <h2>Last Name: {props.lastName}</h2>
+      <h2>Age: {props.age}</h2>
+    </>  
+  )
+}
+
 function App() {
   return (
     <div className="App">
-      <h1>Hello, React!</h1>
+      <Person 
+        name={'John'} 
+        lastName={'Doe'} 
+        age={30}
+        />
+      <Person name={'Amogus'} lastName={'Doe2'} age={23}/>
     </div>
   );
 }
