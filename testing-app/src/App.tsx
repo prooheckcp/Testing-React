0033c7a82b97a2fb934474ffd34e25c4623f3cd1
prooheckcp <61127277+prooheckcp@users.noreply.github.com>
2022-09-22 00:50:06 +0100

    Making movie card layout

--- testing-app/src/App.tsx
+++ testing-app/src/App.tsx
@@ -1,7 +1,17 @@
 import React, { useEffect } from 'react';
+import './App.css';
+import SearchIcon from './search.svg'
 
 const API_KEY : string = "66b19217";
 const API_URL : string = 'http://www.omdbapi.com?apikey=' + API_KEY;
+
+const movieTemplate = {
+    "Title": "Shrek the Third",
+    "Year": "2007",
+    "imdbID": "tt0413267",
+    "Type": "movie",
+    "Poster": "https://m.media-amazon.com/images/M/MV5BOTgyMjc3ODk2MV5BMl5BanBnXkFtZTcwMjY0MjEzMw@@._V1_SX300.jpg"
+}
 
 const App : React.FC = () => {
     
@@ -13,12 +23,40 @@ const App : React.FC = () => {
     }
 
     useEffect(()=>{
-        searchMovies("Shrek")
+        
     }, []);
 
     return(
         <>
-            <h1>Amogus poop</h1>
+            <div className="app">
+                <h1>MovieLand</h1>
+                <div className="search">
+                    <input type="text" placeHolder="Search for movies" value="Superman" onChange={()=>{
+
+                    }}/>
+
+                    <img src={SearchIcon} alt="" onClick={()=>{
+
+                    }}/>
+                </div>
+
+                <div className="container">
+                    <div className="movie">
+                        <div>
+                            <p>{movieTemplate.Year}</p>
+                        </div>
+
+                        <div>
+                            <img src={movieTemplate.Poster !== 'N/A' ? movieTemplate.Poster : 'https://via.placeholder.com/400'} alt={movieTemplate.Title} />
+                        </div>
+
+                        <div>
+                            <span>{movieTemplate.Type}</span>
+                            <h3>{movieTemplate.Title}</h3>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </>    
     );
 }
